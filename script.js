09fec6b1079f3dfdb249677f905f18cada3ccbef
Viviana <vivianaaarenas@gmail.com>
2021-01-11 00:14:11 -0500

    getting the colorblocks to finally link

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ var dayofWeek = moment().format("dddd");
 var dateofMonth = moment().format("MMMM Do YYYY, h:mm a");
 var todaysDate = dayofWeek + ", " + dateofMonth;
 
+const colorcode = $(".colorCode");
+
 $("#currentDay").text(todaysDate);
 
 const saveBtn = $(".saveBtn");
@@ -14,3 +16,32 @@ $(".saveBtn").on("click", function () {
   var plannerTask = $(this).prev().val();
   localStorage.setItem(timeId, plannerTask);
 });
+
+for (var i = 9; i < 18; i++) {
+  var now = moment().format("h");
+  console.log(now);
+
+  var amPm = "am";
+  if (i >= 12) {
+    amPm = "pm";
+  }
+
+  var hour = i;
+  if (i > 12) {
+    hour -= 12;
+  }
+
+  var targetId = "#" + hour + amPm;
+
+  var timeInput = localStorage.getItem(hour + amPm);
+  if (timeInput) {
+    $(targetId).val(timeInput);
+  }
+  if (i < now) {
+    $(targetId).addClass("past");
+  } else if (i > now) {
+    $(targetId).addClass("future");
+  } else {
+    $(targetId).addClass("present");
+  }
+}

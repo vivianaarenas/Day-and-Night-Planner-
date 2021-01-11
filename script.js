682bfcd0cefd006a05c1cd9d42eccf9665dd85e9
Viviana <vivianaaarenas@gmail.com>
2021-01-10 22:51:28 -0500

    local storage

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-// ////// create variable for now displayed in header
+////// create variable for now displayed in header
 var dayofWeek = moment().format("dddd");
 var dateofMonth = moment().format("MMMM Do YYYY, h:mm a");
 var todaysDate = dayofWeek + ", " + dateofMonth;
 
 $("#currentDay").text(todaysDate);
+
+const saveBtn = $(".saveBtn");
+var plannerInput = $(".timeBlocks").val();
+
+$(".saveBtn").on("click", function () {
+  var timeId = $(this).attr("time");
+
+  var plannerTask = $(this).prev().val();
+  localStorage.setItem(timeId, plannerTask);
+});
